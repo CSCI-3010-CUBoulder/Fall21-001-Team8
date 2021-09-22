91d37bb0ed357e8239b21d7d7c1b93e47078cd2c
zeroabstract <yiwu6283@colorado.edu>
2021-09-22 11:01:22 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -20,7 +20,7 @@ std::vector<int> VectorTimesN(std::vector<int> v, int n){
 
 TEST_CASE( "Add n to vector", "[VectorPlusN]" ) {
     std::vector<int> sim{1,2,3,5,7,11};
-    std::vector<int> a{2,3,4,5,8,12};
+    std::vector<int> a{2,3,4,6,8,12};
     std::vector<int> b{6,7,8,10,12,16};
     std::vector<int> c{0,1,2,4,6,10};
     REQUIRE( VectorPlusN(sim,1) == a );
