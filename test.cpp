73624d4a68797d97073fc7f1be5b4f483e05a720
zeroabstract <yiwu6283@colorado.edu>
2021-09-22 10:50:48 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ std::vector<int> VectorPlusN(std::vector<int> v, int n){
     {
         vec[i] = v[i]+n;
     }
-    return n;
+    return v;
 }
 
 // Multiples an integer n with each element of a given vector
@@ -14,14 +14,14 @@ std::vector<int> VectorTimesN(std::vector<int> v, int n){
     {
         vec[i] = v[i]*n;
     }
-    return n;
+    return v;
 }
 
 TEST_CASE( "Add n to vector", "[VectorPlusN]" ) {
-    std::vector<int> sim{1,2,3,5,7,11}
-    std::vector<int> a{2,3,4,5,8,12}
-    std::vector<int> b{6,7,8,10,12,16}
-    std::vector<int> c{0,1,2,4,6,10}
+    std::vector<int> sim{1,2,3,5,7,11};
+    std::vector<int> a{2,3,4,5,8,12};
+    std::vector<int> b{6,7,8,10,12,16};
+    std::vector<int> c{0,1,2,4,6,10};
     REQUIRE( VectorPlusN(sim,1) == a );
     REQUIRE( VectorPlusN(sim,5) == b );
     REQUIRE( VectorPlusN(sim,-1) == c );
