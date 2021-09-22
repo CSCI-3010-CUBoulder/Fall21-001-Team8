63c491b96f6cd45a5931109588c000aea3885f65
zeroabstract <yiwu6283@colorado.edu>
2021-09-22 10:21:06 -0600

    Add files via upload
    test.cpp

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <catch2/catch_test_macros.hpp>
+
+std::vector<int> VectorPlusN(std::vector<int> v, int n){
+    for (int i=0; i<vec.size(); i++)
+    {
+        vec[i] = vec[i]+n;
+    }
+    return n;
+}
+
+// Multiples an integer n with each element of a given vector
+std::vector<int> VectorTimesN(std::vector<int> v, int n){
+    for (int i=0; i<vec.size(); i++)
+    {
+        vec[i] = vec[i]*n;
+    }
+    return n;
+}
+
+TEST_CASE( "Add n to vector", "[VectorPlusN]" ) {
+    std::vector<int> sim{1,2,3,5,7,11}
+    std::vector<int> a{2,3,4,5,8,12}
+    std::vector<int> b{6,7,8,10,12,16}
+    std::vector<int> c{0,1,2,4,6,10}
+    REQUIRE( VectorPlusN(sim,1) == a );
+    REQUIRE( VectorPlusN(sim,5) == 2 );
+    REQUIRE( VectorPlusN(sim,-1) == 6 );
+}
